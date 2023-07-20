9d6e0b48149d45deff9e36d660cea8b756d973ad
Ginno Taimal <ginno.taimal@epn.edu.ec>
2023-07-20 07:58:33 -0500

    feat: Add asignarCalificacion() function

--- calificaciones.c
+++ calificaciones.c
@@ -10,3 +10,18 @@ void inicializarCalificaciones() {
         }
     }
 }
+
+int asignarCalificacion(int estudiante, int evaluacion, int calificacion) {
+    if (estudiante < 0 || estudiante >= NUM_ESTUDIANTES || evaluacion < 0 || evaluacion >= NUM_EVALUACIONES) {
+        return 0; // Índices inválidos
+    }
+
+    //Comprobar calificacion
+    if (calificacion < 0 || calificacion > 100) {
+        return 0; // Calificación inválida
+    }
+
+    calificaciones[estudiante][evaluacion] = calificacion;
+    guardarCalificacionesEnArchivo();
+    return 1; // Asignación exitosa
+}
